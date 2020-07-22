dff4ac70d52537f7304017f542816e8d0b03db27
Sergey <boma385@gmail.com>
2020-07-22 17:43:28 +0300

    init some routes

--- routes/conditioner-route.js
+++ routes/conditioner-route.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const ConditionModel = require('../models/mongoose');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('condition');
-});
+router
+  .get('/', (req, res) => {
+    res.render('condition');
+  })
+  .get('/question', async (req, res) => {
+    const quiz = await ConditionModel.find();
+    res.json(quiz);
+  });
 
 module.exports = router;
