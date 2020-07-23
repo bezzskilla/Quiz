bdd942bcf75b6b8600975a50545b2a0076b9c331
Nikita <ruseev.nikita@gmail.com>
2020-07-23 10:07:42 +0300

    added raw flexbox + some fixes

--- public/index.js
+++ public/index.js
@@ -9,22 +9,24 @@ let closeDialogAboutVentilation = document.querySelector('#closeDialogAboutVenti
 if (conditionButton) {
   conditionButton.addEventListener('click', async e => {
     conditionButton.style.cssText = 'display: none;';
+    dialogAboutCondition.style.cssText = 'display: flex';
     dialogAboutCondition.show(); //показываем диалоговое окно кондиционеров
     const response = await fetch('/condition/question')
-  const resp = await response.json()
+    const resp = await response.json()
 
-  const hbsresponce = await fetch('/hbs/first.hbs')
-  let HBShtml = await hbsresponce.text();
-  let template = Handlebars.compile(HBShtml);
-  let html = template();
+    const hbsresponce = await fetch('/hbs/first.hbs')
+    let HBShtml = await hbsresponce.text();
+    let template = Handlebars.compile(HBShtml);
+    let html = template();
 
-  let forConditionhbs = document.querySelector("#forConditionhbs")
-  forConditionhbs.innerHTML = html;
+    let forConditionhbs = document.querySelector("#forConditionhbs")
+    forConditionhbs.innerHTML = html;
   })
 }
 if (closeDialogAboutCondition) {
   closeDialogAboutCondition.addEventListener('click', async e => {
     conditionButton.style.cssText = '';
+    dialogAboutCondition.style.cssText = 'display: none;';
     dialogAboutCondition.close() //прячем диалоговое окно кондиционеров
   })
 }
@@ -33,11 +35,13 @@ if (closeDialogAboutCondition) {
 if (ventilationButton) {
   ventilationButton.addEventListener('click', async e => {
     ventilationButton.style.cssText = 'display: none;';
+    dialogAboutVentilation.style.cssText = 'display: flex';
     dialogAboutVentilation.show(); //показываем диалоговое окно вентиляции
   })
 }
 if (closeDialogAboutVentilation) {
   closeDialogAboutVentilation.addEventListener('click', async e => {
+    dialogAboutVentilation.style.cssText = 'display: none;';
     ventilationButton.style.cssText = '';
     dialogAboutVentilation.close() //прячем диалоговое окно вентиляции
   })
